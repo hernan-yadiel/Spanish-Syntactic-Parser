fec701c73ee63252bf12894abb332f08507e3728
Yadiel Vélez <yadiel.velez1@upr.edu>
2019-05-11 22:11:21 -0400

    better code documentation

--- parser.pl
+++ parser.pl
@@ -182,6 +182,9 @@ adv0(adv0(hoy), [])  --> [hoy].
 adv0(adv0(aquí), [])  --> [aquí].
 adv0(adv0(allá), [])  --> [allá].
 int0(int0(muy),  [])  --> [muy].
+
+
+
 %%! PREPOSITIONS
 %% p0(p0(preposition), [case])
 p0(p0(con), [obl]) --> [con].
@@ -206,7 +209,7 @@ v0(v0(saludaron),[[pret],[_,3,p],[acc], [dp]]) --> [saludaron].
 
 
 %%! INFLECTION
-% i0(i0(),Features) --> [].
+%% i0(i0(),Features) --> [].
 i0(i0(imperf),[imperf]) --> [].
 i0(i0(pret),[pret]) --> [].
 
@@ -219,6 +222,14 @@ c0(c0(que), [[ip]]) --> [que].
 c0(c0(), [[ip]])    --> [].
 
 
+%%! coordinating conjunctions
+%%! con0(con0(Con), [[Class],[subcategorization]])
+con0(con0(y), [[conjunctiveCoord],[_]]) --> [y].
+con0(con0(e), [[conjunctiveCoord],[_]]) --> [e].
+con0(con0(o), [[disjunctiveCoord],[_]]) --> [o].
+con0(con0(u), [[disjunctiveCoord],[_]]) --> [u].
+
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PHRASE RULES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -227,39 +238,57 @@ c0(c0(), [[ip]])    --> [].
 
 
 %%! NOUNS
-% nbar(nbar(N), [Case,Phi, Class]) 
-% np(np(N), [Case,Phi, F])
+
+%%! nbar(nbar(N), [Case,Phi, Class]) 
+
 nbar(nbar(N), [Case,Phi, Class])     --> n0(N, [Case,Phi, Class, [dp]]).
+%%! nouns assigning genitive case E.g. el telescopio de Irene (Irene's telescope)
 nbar(nbar(N, D), [Case,Phi, Class])  --> n0(N, [Case,Phi, Class, [dp]]), dp(D, [[gen], _]).
 
+%%! np(np(N), [Case,Phi, F])
+
 np(np(N), [Case,Phi, Class])    --> nbar(N, [Case, Phi, Class]). 
+%%! noun phrase + adjective
 np(np(N, A), [Case,Phi, Class]) --> np(N, [Case, Phi, Class]), ap(A, [Case,Phi]).
+%%! adjective + noun phrase
 np(np(A, N), [Case,Phi, Class]) --> ap(A, [Case, Phi]), np(N, [Case,Phi, Class]).
+%%! noun phrase + preposition
 np(np(N, P), [Case,Phi, Class]) --> np(N, [Case, Phi, Class]), pp(P,_).
 
 
 
 %%! ADJECTIVES
-%%  abar(abar(A), [Case, Phi])
-%   ap(ap(A), [Case, Phi])
+
+%%!  abar(abar(A), [Case, Phi])
+
 abar(abar(A), [_, Phi])  --> a0(A, [_, Phi]).
 
+%%! ap(ap(A), [Case, Phi])
+
 ap(ap(A), [_, Phi])      --> abar(A, [_,Phi]).
+%%! intensifier + adjective E.g. very famous
 ap(ap(Int, A), [_, Phi]) --> int0(Int,_), abar(A, [_,Phi]).
+%%! adjective + coordinative conjunction phrase E.g. interesting and famous
 ap(ap(A, Con), [_, Phi]) --> ap(A, [_,Phi]), conp(Con, [_, Phi, [ap]]).
 
 
 
 
 %%! DETERMINERS
-%% dbar(dbar(D), [Case, Phi])
-%% dp(dp(D), [Case, Phi])
+
+%%! dbar(dbar(D), [Case, Phi])
+
+%%! null subject
 dbar(dbar(pro), [Case, Phi])     --> d0(pro, [Case, Phi,[_,_],[]]).
 %%!  personal pronouns
 dbar(dbar(D), [Case, Phi])     --> d0(D, [Case, Phi,[proper, hum],[]]).
 %%! determiner + np (articles)
 dbar(dbar(D, N),[Case2, Phi])  --> d0(D, [Case2, Phi, Class,[np]]), np(N, [Case, Phi, Class]), { subset(Case2, Case) }.
 
+%%! dp(dp(D), [Case, Phi])
+
+dp(dp(D), [Case, Phi]) --> dbar(D, [Case, Phi]).
+%%! determiner phrase and coordinating conjunction
 dp(dp(D, Con), [Case,[Gen, Per, p]]) --> dp(D, [Case, [Gen1, Per1,_]]), conp(Con, [_, [Gen2, Per2,_], [dp]]),
     {
       ((Gen1 == f , Gen2 == f) -> Gen = f ; Gen = m) , %%! if both are feminine then Gen is f, m otherwise
@@ -270,32 +299,41 @@ dp(dp(D, Con), [Case,[Gen, Per, p]]) --> dp(D, [Case, [Gen1, Per1,_]]), conp(Con
       )
     }.
 
-dp(dp(D), [Case, Phi]) --> dbar(D, [Case, Phi]).
-
 
 
 %%! ADVERBS
-%% advbar(advbar(Adv), [features])
-%  advp(advp(Adv), [features])
-advbar(advbar(Adv), F) --> adv0(Adv, F).
-advp(advp(Adv), F)     --> advbar(Adv, F).
-advp(advp(Adv, Con), F)     --> advp(Adv, F), conp(Con, [_, [advp]]).
+
+%%! advbar(advbar(Adv), [features])
+
+advbar(advbar(Adv), F)  --> adv0(Adv, F).
+
+%%!  advp(advp(Adv), [features])
+
+advp(advp(Adv), F)      --> advbar(Adv, F).
+%%! adverbial phrase and coordinating conjunction
+advp(advp(Adv, Con), F) --> advp(Adv, F), conp(Con, [_, [advp]]).
 
 
 
 %%! PREPOSITONS
-% pbar(pbar(P, D), [Case])
-% pp(pp(P), [Case])
-pbar(pbar(P, D),_) --> p0(P,Case1), dp(D,[Case,_]), { subset(Case1, Case) }.
+
+%%! pbar(pbar(P, D), [Case])
+
+pbar(pbar(P, D),_) --> p0(P,Case1), dp(D,[Case2,_]), { subset(Case1, Case2) }. %%! checks if oblique case is in the DP case list, because there can be DPs with multiple cases.
+
+%%! pp(pp(P), [Case])
 
 pp(pp(P), F) --> pbar(P, F).
+%%! prepositional phrase with a coordinating conjunction
 pp(pp(P, Con), F) --> pp(P, F), conp(Con, [F, [pp]]).
 
 
 
 %%! VERBS
-%% vbar(vbar(V),[Tense,Phi])
-% unaccusative verbs
+
+%%! vbar(vbar(V),[Tense,Phi])
+
+%%! unaccusative verbs
 vbar(vbar(V),[T,Phi])     --> v0(V,[T,Phi,[], []]).
 %%! clitic + verb
 vbar(vbar(D, V),[T,Phi])  --> d0(D, [Case,_,[_,_], [v0]]), v0(V,[T,Phi, Case, [dp]]).
@@ -308,16 +346,23 @@ vbar(vbar(V, P),[T,Phi])  --> v0(V,[T,Phi,_, [pp]]), pp(P,_).
 %%! verb + ap complement(copulative verb)
 vbar(vbar(V, A), [T,Phi]) --> v0(V, [T,Phi,[],[_]]), ap(A, [_, Phi]).
 
+%%! vp(vp(V),[Tense,Phi])
+
 vp(vp(V), [T,Phi])      --> vbar(V, [T,Phi]).
+%%! prepositional phrase adjunction to verb phrases
 vp(vp(V, P), [T,Phi])   --> vp(V, [T,Phi]), pp(P,_).
+%%! adverbial phrase adjunction to verb phrases
 vp(vp(V, Adv), [T,Phi]) --> vp(V, [T,Phi]), advp(Adv,_).
 
 
 
 %%! INFLECTION
-% ibar(ibar(I, V), [Tense, Phi])
-% ip(ip(D, I), [Case, Phi, Tense])
+
+%%! ibar(ibar(I, V), [Tense, Phi])
+
 ibar(ibar(I, V), [T, Phi])   --> i0(I, T), vp(V, [T, Phi]).
+
+%%! ip(ip(D, I), [Case, Phi, Tense])
 
 ip(ip(D, I), [Case, Phi, T]) --> dp(D, [Case, Phi]), ibar(I, [T, Phi]).
 ip(ip(Adv, I), [_, _])       --> advp(Adv, _), ip(I,_).
@@ -325,19 +370,31 @@ ip(ip(Adv, I), [_, _])       --> advp(Adv, _), ip(I,_).
 
 
 %%! COMPLEMENTZIERS
-% cbar(cbar(C, I), Features)
-% cp(cp(C), Features)
+
+%%! cbar(cbar(C, I), Features)
+
 cbar(cbar(C, I),_) --> c0(C,_), ip(I, _).
+
+%%! cp(cp(C), Features)
 
 cp(cp(C),_) --> cbar(C,_).
 
-%%! COORDINATE CONJUNCTIONS
-con0(con(y), [[conjunctiveCoord],[_]]) --> [y].
-con0(con(o), [[disjunctiveCoord],[_]]) --> [o].
 
+
+%%! COORDINATE CONJUNCTIONS
+
+%%! conbar(conbar(Con), [F, [subcategorization]])
+
+%%! coordinating conjunction for determiner phrase
 conbar(conbar(Con, D), [Case, Phi, [dp]]) --> con0(Con,_), dp(D, [Case, Phi]).
+%%! coordinating conjunction for adjective phrase
 conbar(conbar(Con, A), [Case, Phi, [ap]]) --> con0(Con,_), ap(A, [Case, Phi]).
+%%! coordinating conjunction for adverbial phrase
 conbar(conbar(Con, Adv), [F, [advp]]) --> con0(Con,_), advp(Adv, F).
+%%! coordinating conjunction for prepositional phrase
 conbar(conbar(Con, P), [F, [pp]]) --> con0(Con,_), pp(P, F).
 
+%%! conp(conbar(Con), F)
+
 conp(conp(Con), F) --> conbar(Con, F).
+
